f6c3f775805943c3da21953e56dd4c0136d745a6
Renan Zambon <renanrzambon@gmail.com>
2024-01-15 17:49:44 -0300

    Unários

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,31 +1,24 @@
 public class Operadores {
     public static void main(String[] args) {
-        //classe Operadores.java
-String nomeCompleto = "LINGUAGEM" + "JAVA";
+        int numero = 5;
 		
-//qual o resultado das expressoes abaixo?
-String concatenacao ="?"; 
-
-System.out.println(concatenacao);
-
-concatenacao = 1+1+1+"1";
-
-System.out.println(concatenacao);
-
-concatenacao = 1+"1"+1+1;
-
-System.out.println(concatenacao);
-
-concatenacao = 1+"1"+1+"1";
-
-System.out.println(concatenacao);
-
-concatenacao = "1"+1+1+1;
-
-System.out.println(concatenacao);
-
-concatenacao = "1"+(1+1+1);
-
-        System.out.println(concatenacao);
+        //Imprimindo o numero negativo
+        System.out.println(- numero);
+        
+        //incrementando numero em mais 1 numero, imprime 6
+        numero ++;
+        System.out.println(numero);
+        
+        //incrementando numero em mais 1 numero, imprime 7
+        System.out.println(numero ++);// ops algo de errado não está certo
+        
+        System.out.println(numero);// agora sim, numero virou 7
+        
+        //ordem de precedencia conta aqui
+        System.out.println(++ numero);
+        
+        boolean verdadeiro = true;
+        
+        System.out.println("Inverteu " + !verdadeiro);
     }
 }
