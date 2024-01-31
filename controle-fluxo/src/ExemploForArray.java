78aa744c3db9db5ebe9ffb07c7d39cf006cbbe6e
Renan Zambon <renanrzambon@gmail.com>
2024-01-31 00:05:56 -0300

    Controle de repetição for

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,12 @@
+// ExemploFor.java
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+    
+        // em arrays o indice inicia em ZERO
+        for (int x=0; x < alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+    
+    }
+    }
